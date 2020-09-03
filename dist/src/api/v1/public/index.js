a210629bfcfebfde1141927564503fc8cb67f029
irohafox <matsukawa@grabss.co.jp>
2020-09-03 19:24:14 +0900

    Feat: ルーム単位でのpeerId一覧取得API実装

--- dist/src/api/v1/public/index.js
+++ dist/src/api/v1/public/index.js
@@ -6,20 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 exports.default = ({ config, realm }) => {
     const app = express_1.default.Router();
-    // Retrieve guaranteed random ID.
     app.get("/id", (_, res) => {
         res.contentType("html");
         res.send(realm.generateClientId(config.generateClientId));
     });
-    // Get a list of all peers for a key, enabled by the `allowDiscovery` flag.
-    // app.get("/peers", (_, res: express.Response) => {
-    //   if (config.allow_discovery) {
-    //     const clientsIds = realm.getClientsIds();
-    //
-    //     return res.send(clientsIds);
-    //   }
-    //
-    //   res.sendStatus(401);
-    // });
+    app.get("/peers", ({ query }, res) => {
+        const room = realm.getRoomByName(query.roomName);
+        const clientsIds = room.getClientsIds();
+        return res.send(clientsIds);
+    });
     return app;
 };
--- src/api/v1/public/index.ts
+++ src/api/v1/public/index.ts
@@ -6,23 +6,18 @@ export default ({ config, realm }: {
   config: IConfig, realm: IRealm
 }): express.Router => {
   const app = express.Router();
-
-  // Retrieve guaranteed random ID.
+  
   app.get("/id", (_, res: express.Response) => {
     res.contentType("html");
     res.send(realm.generateClientId(config.generateClientId));
   });
 
-  // Get a list of all peers for a key, enabled by the `allowDiscovery` flag.
-  // app.get("/peers", (_, res: express.Response) => {
-  //   if (config.allow_discovery) {
-  //     const clientsIds = realm.getClientsIds();
-  //
-  //     return res.send(clientsIds);
-  //   }
-  //
-  //   res.sendStatus(401);
-  // });
+  app.get("/peers", ({ query }, res: express.Response) => {
+    const room = realm.getRoomByName(query.roomName)!
+    const clientsIds = room.getClientsIds();
+
+    return res.send(clientsIds);
+  });
 
   return app;
 };
