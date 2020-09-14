8d9e34064768974c7213e7b95dfb30e089aba93c
irohafox <matsukawa@grabss.co.jp>
2020-09-14 16:52:48 +0900

    Fix: コミット漏れ

--- dist/src/models/room.js
+++ dist/src/models/room.js
@@ -4,7 +4,7 @@ class Room {
     constructor({ name }) {
         this.clients = new Map();
         this.name = name;
-        this.password = "test";
+        this.password = "";
     }
     getName() {
         return this.name;
