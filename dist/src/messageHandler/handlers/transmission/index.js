4611985ae508ef46274e1929d46c5f121d2668f4
irohafox <matsukawa@grabss.co.jp>
2020-10-02 10:37:47 +0900

    Fix: node異常終了対応

--- dist/src/messageHandler/handlers/transmission/index.js
+++ dist/src/messageHandler/handlers/transmission/index.js
@@ -7,84 +7,86 @@ exports.TransmissionHandler = ({ realm }) => {
         const srcId = message.src;
         const dstId = message.dst;
         const room = realm.getRoomByClientId(dstId);
-        const destinationClient = room.getClientById(dstId);
-        // User is connected!
-        if (destinationClient) {
-            const socket = destinationClient.getSocket();
-            try {
-                if (socket) {
-                    const data = JSON.stringify(message);
-                    if (type === enums_1.MessageType.KNOCK) {
-                        const knockRoomName = message.payload.roomName;
-                        const knockRoom = realm.getRoomByName(knockRoomName);
-                        socket.send(JSON.stringify({
-                            type: enums_1.MessageType.KNOCK_REPLY,
-                            payload: {
-                                roomName: knockRoomName,
-                                isExists: knockRoom ? true : false,
-                                isRequiredPassword: knockRoom ? knockRoom.isRequiredPassword() : false
-                            }
-                        }));
-                    }
-                    else if (type === enums_1.MessageType.SET_PASSWORD) {
-                        const newPassword = message.payload.password;
-                        room.setPassword(newPassword);
-                        room.getClients().forEach(otherClient => {
-                            handle(otherClient, {
-                                type: enums_1.MessageType.PASSWORD_CHANGED,
-                                src: destinationClient.getId(),
-                                dst: otherClient.getId(),
+        if (room) {
+            const destinationClient = room.getClientById(dstId);
+            // User is connected!
+            if (destinationClient) {
+                const socket = destinationClient.getSocket();
+                try {
+                    if (socket) {
+                        const data = JSON.stringify(message);
+                        if (type === enums_1.MessageType.KNOCK) {
+                            const knockRoomName = message.payload.roomName;
+                            const knockRoom = realm.getRoomByName(knockRoomName);
+                            socket.send(JSON.stringify({
+                                type: enums_1.MessageType.KNOCK_REPLY,
                                 payload: {
-                                    remove: newPassword ? false : true
+                                    roomName: knockRoomName,
+                                    isExists: knockRoom ? true : false,
+                                    isRequiredPassword: knockRoom ? knockRoom.isRequiredPassword() : false
                                 }
+                            }));
+                        }
+                        else if (type === enums_1.MessageType.SET_PASSWORD) {
+                            const newPassword = message.payload.password;
+                            room.setPassword(newPassword);
+                            room.getClients().forEach(otherClient => {
+                                handle(otherClient, {
+                                    type: enums_1.MessageType.PASSWORD_CHANGED,
+                                    src: destinationClient.getId(),
+                                    dst: otherClient.getId(),
+                                    payload: {
+                                        remove: newPassword ? false : true
+                                    }
+                                });
                             });
-                        });
+                        }
+                        else {
+                            socket.send(data);
+                        }
                     }
                     else {
-                        socket.send(data);
+                        // Neither socket no res available. Peer dead?
+                        throw new Error("Peer dead");
                     }
                 }
-                else {
-                    // Neither socket no res available. Peer dead?
-                    throw new Error("Peer dead");
+                catch (e) {
+                    // This happens when a peer disconnects without closing connections and
+                    // the associated WebSocket has not closed.
+                    // Tell other side to stop trying.
+                    if (socket) {
+                        socket.close();
+                    }
+                    else {
+                        room.removeClientById(destinationClient.getId());
+                        if (room.getClientsIds().length === 0) {
+                            realm.removeRoomByName(room.getName());
+                        }
+                    }
+                    handle(client, {
+                        type: enums_1.MessageType.LEAVE,
+                        src: dstId,
+                        dst: srcId
+                    });
                 }
             }
-            catch (e) {
-                // This happens when a peer disconnects without closing connections and
-                // the associated WebSocket has not closed.
-                // Tell other side to stop trying.
-                if (socket) {
-                    socket.close();
+            else {
+                // Wait for this client to connect/reconnect (XHR) for important
+                // messages.
+                const ignoredTypes = [enums_1.MessageType.LEAVE, enums_1.MessageType.EXPIRE];
+                if (!ignoredTypes.includes(type) && dstId) {
+                    realm.addMessageToQueue(dstId, message);
                 }
-                else {
-                    room.removeClientById(destinationClient.getId());
+                else if (type === enums_1.MessageType.LEAVE && !dstId) {
+                    room.removeClientById(srcId);
                     if (room.getClientsIds().length === 0) {
                         realm.removeRoomByName(room.getName());
                     }
                 }
-                handle(client, {
-                    type: enums_1.MessageType.LEAVE,
-                    src: dstId,
-                    dst: srcId
-                });
-            }
-        }
-        else {
-            // Wait for this client to connect/reconnect (XHR) for important
-            // messages.
-            const ignoredTypes = [enums_1.MessageType.LEAVE, enums_1.MessageType.EXPIRE];
-            if (!ignoredTypes.includes(type) && dstId) {
-                realm.addMessageToQueue(dstId, message);
-            }
-            else if (type === enums_1.MessageType.LEAVE && !dstId) {
-                room.removeClientById(srcId);
-                if (room.getClientsIds().length === 0) {
-                    realm.removeRoomByName(room.getName());
+                else {
+                    // Unavailable destination specified with message LEAVE or EXPIRE
+                    // Ignore
                 }
-            }
-            else {
-                // Unavailable destination specified with message LEAVE or EXPIRE
-                // Ignore
             }
         }
         return true;
--- src/messageHandler/handlers/transmission/index.ts
+++ src/messageHandler/handlers/transmission/index.ts
@@ -9,82 +9,85 @@ export const TransmissionHandler = ({ realm }: { realm: IRealm; }): (client: ICl
     const srcId = message.src;
     const dstId = message.dst;
 
-    const room = realm.getRoomByClientId(dstId)!;
-    const destinationClient = room.getClientById(dstId);
+    const room = realm.getRoomByClientId(dstId);
 
-    // User is connected!
-    if (destinationClient) {
-      const socket = destinationClient.getSocket();
-      try {
-        if (socket) {
-          const data = JSON.stringify(message);
+    if (room) {
+      const destinationClient = room.getClientById(dstId);
 
-          if (type === MessageType.KNOCK) {
-            const knockRoomName = message.payload.roomName
-            const knockRoom = realm.getRoomByName(knockRoomName)
+      // User is connected!
+      if (destinationClient) {
+        const socket = destinationClient.getSocket();
+        try {
+          if (socket) {
+            const data = JSON.stringify(message);
 
-            socket.send(JSON.stringify({
-              type: MessageType.KNOCK_REPLY,
-              payload: {
-                roomName: knockRoomName,
-                isExists: knockRoom ? true : false,
-                isRequiredPassword: knockRoom ? knockRoom.isRequiredPassword() : false
-              }
-            }))
-          } else if (type === MessageType.SET_PASSWORD) {
-            const newPassword = message.payload.password
-            room.setPassword(newPassword)
-            room.getClients().forEach(otherClient => {
-              handle(otherClient, {
-                type: MessageType.PASSWORD_CHANGED,
-                src: destinationClient.getId(),
-                dst: otherClient.getId(),
+            if (type === MessageType.KNOCK) {
+              const knockRoomName = message.payload.roomName
+              const knockRoom = realm.getRoomByName(knockRoomName)
+
+              socket.send(JSON.stringify({
+                type: MessageType.KNOCK_REPLY,
                 payload: {
-                  remove: newPassword ? false : true
+                  roomName: knockRoomName,
+                  isExists: knockRoom ? true : false,
+                  isRequiredPassword: knockRoom ? knockRoom.isRequiredPassword() : false
                 }
+              }))
+            } else if (type === MessageType.SET_PASSWORD) {
+              const newPassword = message.payload.password
+              room.setPassword(newPassword)
+              room.getClients().forEach(otherClient => {
+                handle(otherClient, {
+                  type: MessageType.PASSWORD_CHANGED,
+                  src: destinationClient.getId(),
+                  dst: otherClient.getId(),
+                  payload: {
+                    remove: newPassword ? false : true
+                  }
+                })
               })
-            })
+            } else {
+              socket.send(data);
+            }
           } else {
-            socket.send(data);
+            // Neither socket no res available. Peer dead?
+            throw new Error("Peer dead");
           }
-        } else {
-          // Neither socket no res available. Peer dead?
-          throw new Error("Peer dead");
+        } catch (e) {
+          // This happens when a peer disconnects without closing connections and
+          // the associated WebSocket has not closed.
+          // Tell other side to stop trying.
+          if (socket) {
+            socket.close();
+          } else {
+            room.removeClientById(destinationClient.getId());
+            if (room.getClientsIds().length === 0) {
+              realm.removeRoomByName(room.getName());
+            }
+          }
+
+          handle(client, {
+            type: MessageType.LEAVE,
+            src: dstId,
+            dst: srcId
+          });
         }
-      } catch (e) {
-        // This happens when a peer disconnects without closing connections and
-        // the associated WebSocket has not closed.
-        // Tell other side to stop trying.
-        if (socket) {
-          socket.close();
-        } else {
-          room.removeClientById(destinationClient.getId());
+      } else {
+        // Wait for this client to connect/reconnect (XHR) for important
+        // messages.
+        const ignoredTypes = [MessageType.LEAVE, MessageType.EXPIRE];
+
+        if (!ignoredTypes.includes(type) && dstId) {
+          realm.addMessageToQueue(dstId, message);
+        } else if (type === MessageType.LEAVE && !dstId) {
+          room.removeClientById(srcId);
           if (room.getClientsIds().length === 0) {
             realm.removeRoomByName(room.getName());
           }
+        } else {
+          // Unavailable destination specified with message LEAVE or EXPIRE
+          // Ignore
         }
-
-        handle(client, {
-          type: MessageType.LEAVE,
-          src: dstId,
-          dst: srcId
-        });
-      }
-    } else {
-      // Wait for this client to connect/reconnect (XHR) for important
-      // messages.
-      const ignoredTypes = [MessageType.LEAVE, MessageType.EXPIRE];
-
-      if (!ignoredTypes.includes(type) && dstId) {
-        realm.addMessageToQueue(dstId, message);
-      } else if (type === MessageType.LEAVE && !dstId) {
-        room.removeClientById(srcId);
-        if (room.getClientsIds().length === 0) {
-          realm.removeRoomByName(room.getName());
-        }
-      } else {
-        // Unavailable destination specified with message LEAVE or EXPIRE
-        // Ignore
       }
     }
 
